db6d6b81b98ee7416c4bed2819556191f811210e
hossein <hossein.h.a770227@gmail.com>
2024-11-21 12:45:34 +0330

    add admin for portfolio model

--- home/admin.py
+++ home/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from jalali_date.admin import ModelAdminJalaliMixin
 
 from .models import Portfolio
 
 
 @admin.register(Portfolio)
-class PortfolioAdmin(admin.ModelAdmin):
+class PortfolioAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
    list_display = ['name_customer', 'date_time_created']
+
+
+
+   admin.site.unregister(User)
+admin.site.unregister(Group)
--- home/models.py
+++ home/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from django.utils import timezone
 from django.utils.translation import gettext as _
+
 
 
 class Portfolio(models.Model):
     name_customer = models.CharField(_("customer name"), max_length=255, blank=True)
     image = models.ImageField(verbose_name=_('image'), upload_to='cover/')
-    date_time_created = models.DateTimeField(_("date created"), auto_now_add=True)
+    date_time_created = models.DateTimeField(_("date created"), default=timezone.now)
 
     class Meta:
         verbose_name = _('portfolio')
